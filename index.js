57c06e698f30ad37df98687cc7634a996c6b3564
Sydney Michael <m13s19madden@gmail.com>
2020-04-05 19:36:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
-dodger.style.backgroundColor = "#000000"
+//dodger.style.backgroundColor = "#000000"
+
+dodger.style.backgroundColor = '#FF69B4'
